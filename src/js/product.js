3a4c54359e07b976b2aaee53daa68f508b0127ed
kendychae@gmail.com <kendychae@gmail.com>
2025-07-04 18:41:00 -0600

    Fix add-to-cart functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the existing cart from localStorage, or use an empty array
+  let cart = getLocalStorage("so-cart") || [];
+  // Add the new product
+  cart.push(product);
+  // Save it back to localStorage
+  setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
+// Add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
